e29e34ebb76381f3aa3efc9289c295f756c3edbd
alam1533 <alam15333@gmail.com>
2022-07-14 16:00:15 +0100

    make another page called market and set with data of dictionary list

--- market.py
+++ market.py
@@ -2,8 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/') # url that navigate through html code. Called route or views
+@app.route('/')  # url that navigate through html code. Called route or views
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    # send parameter as key name
+    return render_template('market.html', items=items)
